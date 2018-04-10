62764126f407db826c053daaa5117e4a5f9e08d8
zhangxianhua01 <zhangxianhua01@baidu.com>
2018-04-10 23:07:50 +0800

    Initial commit

---
+++ app/src/main/java/com/hteng/hongkong/map/MapView.java
@@ -0,0 +1,161 @@
+package com.hteng.hongkong.map;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.view.GestureDetectorCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+/**
+ * Created by baby on 2017/5/31.
+ */
+
+public class MapView extends View {
+    private Context context;
+    private List<ProviceItem> itemList;
+    private float scale=1.3f;
+    private int[] colorArray = new int[]{0xFF239BD7, 0xFF30A9E5, 0xFF80CBF1, 0xFFFFFFFF};
+    private Paint paint;
+    private ProviceItem selectItem;
+    private GestureDetectorCompat gestureDetectorCompat;
+    public MapView(Context context) {
+        this(context,null);
+
+    }
+
+    public MapView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    private void init(Context context) {
+        this.context=context;
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        loadThread.start();
+        gestureDetectorCompat=new GestureDetectorCompat(context,new GestureDetector.SimpleOnGestureListener(){
+            @Override
+            public boolean onDown(MotionEvent e) {
+                handleTouch(e.getX(),e.getY());
+                return true;
+            }
+        });
+    }
+
+
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (itemList!=null) {
+            canvas.save();
+            canvas.scale(scale, scale);
+            for (ProviceItem item : itemList) {
+                if (item != selectItem) {
+                    item.drawItem(canvas,paint,false);
+                }
+            }
+            if (selectItem != null) {
+                selectItem.drawItem(canvas,paint,true);
+            }
+        }
+    }
+    private void handleTouch(float x, float y) {
+        if (itemList == null) {
+            return;
+        }
+        ProviceItem tmpItem=null;
+        for ( ProviceItem item : itemList) {
+            if (item.isTouch((int) (x / scale), (int) (y / scale))) {
+                tmpItem=item;
+                break;
+            }
+        }
+        if (tmpItem != null) {
+            selectItem=tmpItem;
+            postInvalidate();
+        }
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        handleTouch(event.getX(),event.getY());
+        return true;
+    }
+
+    private Thread loadThread=new Thread(){
+        @Override
+        public void run() {
+            List<ProviceItem> list = new ArrayList<>();
+            InputStream inputStream=context.getResources().openRawResource(R.raw.hong_kong_map);
+            try {
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();  //取得DocumentBuilderFactory实例
+                DocumentBuilder builder = factory.newDocumentBuilder(); //从factory获取DocumentBuilder实例
+                Document doc = builder.parse(inputStream);   //解析输入流 得到Document实例
+                Element rootElement = doc.getDocumentElement();
+                NodeList items = rootElement.getElementsByTagName("path");
+                for (int i = 0; i < items.getLength(); i++) {
+                    Element element= (Element) items.item(i);
+                    String pathData=element.getAttribute("android:pathData");
+                    Log.i("tuch","pathData   "+ pathData );
+                    Path path = PathParser.createPathFromPathData(pathData);
+                    ProviceItem proviceItem = new ProviceItem(path);
+                    list.add(proviceItem);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            itemList=list;
+            handler.sendEmptyMessage(1);
+        }
+    };
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if (itemList == null) {
+                return;
+            }
+            int totalNumber=itemList.size();
+            for (int i=0;i<totalNumber; i++) {
+                int color = Color.WHITE;
+                int flag =  i %4;
+                switch (flag) {
+                    case 1:
+                        color = colorArray[0];
+                        break;
+                    case 2:
+                        color = colorArray[1];
+                        break;
+                    case 3:
+                        color = colorArray[2];
+                        break;
+                    default:
+                        color = Color.WHITE;
+                        break;
+                }
+                itemList.get(i).setDrawColor(color);
+            }
+            postInvalidate();
+        }
+    };
+
+}
---
+++ app/src/main/java/com/hteng/hongkong/map/ProviceItem.java
@@ -0,0 +1,74 @@
+package com.hteng.hongkong.map;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.graphics.Region;
+
+/**
+ * Created by baby on 2017/5/31.
+ */
+
+public class ProviceItem {
+
+    protected Path path; // 绘制路径
+
+    private int drawColor; // 绘制颜色
+
+    public ProviceItem(Path path) {
+        this.path = path;
+    }
+
+    void drawItem(Canvas canvas, Paint paint, boolean isSelect) {
+        if (isSelect) {
+            paint.setStrokeWidth(2);
+            paint.setColor(Color.BLACK);
+            paint.setStyle(Paint.Style.FILL);
+            paint.setShadowLayer(8, 0, 0, 0xffffff);
+            canvas.drawPath(path, paint);
+            paint.clearShadowLayer();
+            paint.setColor(drawColor);
+            paint.setStyle(Paint.Style.FILL);
+            paint.setStrokeWidth(2);
+            canvas.drawPath(path, paint);
+        } else {
+            // 非选中时，绘制描边效果
+            paint.clearShadowLayer();
+            paint.setStrokeWidth(1);
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(drawColor);
+            canvas.drawPath(path, paint);
+
+            paint.setStyle(Paint.Style.STROKE);
+            int strokeColor = 0xFFD0E8F4;
+            paint.setColor(strokeColor);
+            canvas.drawPath(path, paint);
+        }
+    }
+
+    public boolean isTouch(int x, int y) {
+        RectF rectF = new RectF();
+        path.computeBounds(rectF, true);
+        Region region = new Region();
+        region.setPath(path, new Region((int) rectF.left, (int) rectF.top, (int) rectF.right, (int) rectF.bottom));
+        return region.contains(x, y);
+    }
+
+    public Path getPath() {
+        return path;
+    }
+
+    public void setPath(Path path) {
+        this.path = path;
+    }
+
+    public int getDrawColor() {
+        return drawColor;
+    }
+
+    public void setDrawColor(int drawColor) {
+        this.drawColor = drawColor;
+    }
+}
